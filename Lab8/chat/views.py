492657283a49389a2389a583c40d81e71cadca9d
JakubPikus <kub943@gmail.com>
2021-01-24 04:26:58 +0100

    Lab 8, 9, 10 - zrzuty

--- Lab8/chat/views.py
+++ Lab8/chat/views.py
@@ -8,3 +8,9 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+def worker1(request):
+     return render(request, 'webWorker/worker1.html', {})
+
+def worker2(request):
+     return render(request, 'webWorker/worker2.html', {})
--- Lab8/mysite/urls.py
+++ Lab8/mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from chat import views
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls),
+    path('worker1/', views.worker1),
+    path('worker2/', views.worker2),
+    
 ]
